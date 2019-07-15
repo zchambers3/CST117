8f6720e88bce6c4bd366680b74fc5f38d6ac4d3e
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-07-14 20:46:51 -0700

    Add files via upload

---
+++ Week 1/Exercise2/ProgrammingExercise-2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ProgrammingExercise_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stateListBox = new System.Windows.Forms.ListBox();
+            this.blueRadioButton = new System.Windows.Forms.RadioButton();
+            this.pinkRadioButton = new System.Windows.Forms.RadioButton();
+            this.nameCheckBox = new System.Windows.Forms.CheckBox();
+            this.dateCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.normalRadioButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.timeZoneLabel = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // stateListBox
+            // 
+            this.stateListBox.FormattingEnabled = true;
+            this.stateListBox.Items.AddRange(new object[] {
+            "California",
+            "Delaware",
+            "Hawaii",
+            "Wyoming",
+            "Texas"});
+            this.stateListBox.Location = new System.Drawing.Point(15, 54);
+            this.stateListBox.Name = "stateListBox";
+            this.stateListBox.Size = new System.Drawing.Size(120, 69);
+            this.stateListBox.TabIndex = 0;
+            this.stateListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // blueRadioButton
+            // 
+            this.blueRadioButton.AutoSize = true;
+            this.blueRadioButton.Location = new System.Drawing.Point(6, 33);
+            this.blueRadioButton.Name = "blueRadioButton";
+            this.blueRadioButton.Size = new System.Drawing.Size(46, 17);
+            this.blueRadioButton.TabIndex = 1;
+            this.blueRadioButton.TabStop = true;
+            this.blueRadioButton.Text = "Blue";
+            this.blueRadioButton.UseVisualStyleBackColor = true;
+            this.blueRadioButton.CheckedChanged += new System.EventHandler(this.blueRadioButton_CheckedChanged);
+            // 
+            // pinkRadioButton
+            // 
+            this.pinkRadioButton.AutoSize = true;
+            this.pinkRadioButton.Location = new System.Drawing.Point(6, 65);
+            this.pinkRadioButton.Name = "pinkRadioButton";
+            this.pinkRadioButton.Size = new System.Drawing.Size(46, 17);
+            this.pinkRadioButton.TabIndex = 2;
+            this.pinkRadioButton.TabStop = true;
+            this.pinkRadioButton.Text = "Pink\r\n";
+            this.pinkRadioButton.UseVisualStyleBackColor = true;
+            this.pinkRadioButton.CheckedChanged += new System.EventHandler(this.pinkRadioButton_CheckedChanged);
+            // 
+            // nameCheckBox
+            // 
+            this.nameCheckBox.AutoSize = true;
+            this.nameCheckBox.Location = new System.Drawing.Point(6, 34);
+            this.nameCheckBox.Name = "nameCheckBox";
+            this.nameCheckBox.Size = new System.Drawing.Size(54, 17);
+            this.nameCheckBox.TabIndex = 3;
+            this.nameCheckBox.Text = "Name";
+            this.nameCheckBox.UseVisualStyleBackColor = true;
+            this.nameCheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // dateCheckBox
+            // 
+            this.dateCheckBox.AutoSize = true;
+            this.dateCheckBox.Location = new System.Drawing.Point(6, 77);
+            this.dateCheckBox.Name = "dateCheckBox";
+            this.dateCheckBox.Size = new System.Drawing.Size(49, 17);
+            this.dateCheckBox.TabIndex = 4;
+            this.dateCheckBox.Text = "Date";
+            this.dateCheckBox.UseVisualStyleBackColor = true;
+            this.dateCheckBox.CheckedChanged += new System.EventHandler(this.dateCheckBox_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nameCheckBox);
+            this.groupBox1.Controls.Add(this.dateCheckBox);
+            this.groupBox1.Location = new System.Drawing.Point(31, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Select Details";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.normalRadioButton);
+            this.groupBox2.Controls.Add(this.blueRadioButton);
+            this.groupBox2.Controls.Add(this.pinkRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(37, 325);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 128);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Select Background Color";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // normalRadioButton
+            // 
+            this.normalRadioButton.AutoSize = true;
+            this.normalRadioButton.Location = new System.Drawing.Point(6, 96);
+            this.normalRadioButton.Name = "normalRadioButton";
+            this.normalRadioButton.Size = new System.Drawing.Size(58, 17);
+            this.normalRadioButton.TabIndex = 3;
+            this.normalRadioButton.TabStop = true;
+            this.normalRadioButton.Text = "Normal";
+            this.normalRadioButton.UseVisualStyleBackColor = true;
+            this.normalRadioButton.CheckedChanged += new System.EventHandler(this.normalRadioButton_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Select a label and I will give you the time zone of the state. \r\n";
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(323, 65);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(61, 13);
+            this.outputDescriptionLabel.TabIndex = 10;
+            this.outputDescriptionLabel.Text = "Time Zone:";
+            // 
+            // timeZoneLabel
+            // 
+            this.timeZoneLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeZoneLabel.Location = new System.Drawing.Point(464, 64);
+            this.timeZoneLabel.Name = "timeZoneLabel";
+            this.timeZoneLabel.Size = new System.Drawing.Size(100, 23);
+            this.timeZoneLabel.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(246, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Print";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.timeZoneLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.stateListBox);
+            this.Name = "Form1";
+            this.Text = "Time Zone Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox stateListBox;
+        private System.Windows.Forms.RadioButton blueRadioButton;
+        private System.Windows.Forms.RadioButton pinkRadioButton;
+        private System.Windows.Forms.CheckBox nameCheckBox;
+        private System.Windows.Forms.CheckBox dateCheckBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton normalRadioButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Label timeZoneLabel;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
