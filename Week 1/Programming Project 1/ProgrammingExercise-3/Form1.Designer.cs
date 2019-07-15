cf1a096790731343eccb0b3c0a7fcc79ea0e6384
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-07-14 20:50:28 -0700

    Add files via upload

---
+++ Week 1/Programming Project 1/ProgrammingExercise-3/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CST_117_Week_1_Assignment_3_and_Beyond
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.earthWeightLabel = new System.Windows.Forms.Label();
+            this.earthWeightTextBox = new System.Windows.Forms.TextBox();
+            this.marsWeightLabel = new System.Windows.Forms.Label();
+            this.marsWeightTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // earthWeightLabel
+            // 
+            this.earthWeightLabel.AutoSize = true;
+            this.earthWeightLabel.Location = new System.Drawing.Point(13, 17);
+            this.earthWeightLabel.Name = "earthWeightLabel";
+            this.earthWeightLabel.Size = new System.Drawing.Size(141, 13);
+            this.earthWeightLabel.TabIndex = 0;
+            this.earthWeightLabel.Text = "Enter your weight on Earth : ";
+            // 
+            // earthWeightTextBox
+            // 
+            this.earthWeightTextBox.Location = new System.Drawing.Point(161, 13);
+            this.earthWeightTextBox.Name = "earthWeightTextBox";
+            this.earthWeightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.earthWeightTextBox.TabIndex = 1;
+            // 
+            // marsWeightLabel
+            // 
+            this.marsWeightLabel.AutoSize = true;
+            this.marsWeightLabel.Location = new System.Drawing.Point(16, 49);
+            this.marsWeightLabel.Name = "marsWeightLabel";
+            this.marsWeightLabel.Size = new System.Drawing.Size(116, 13);
+            this.marsWeightLabel.TabIndex = 2;
+            this.marsWeightLabel.Text = "Your Weight on Mars : ";
+            // 
+            // marsWeightTextBox
+            // 
+            this.marsWeightTextBox.Location = new System.Drawing.Point(161, 49);
+            this.marsWeightTextBox.Name = "marsWeightTextBox";
+            this.marsWeightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.marsWeightTextBox.TabIndex = 3;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(161, 90);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 4;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 121);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.marsWeightTextBox);
+            this.Controls.Add(this.marsWeightLabel);
+            this.Controls.Add(this.earthWeightTextBox);
+            this.Controls.Add(this.earthWeightLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label earthWeightLabel;
+        private System.Windows.Forms.TextBox earthWeightTextBox;
+        private System.Windows.Forms.Label marsWeightLabel;
+        private System.Windows.Forms.TextBox marsWeightTextBox;
+        private System.Windows.Forms.Button convertButton;
+    }
+}
+
---
+++ Week 1/Programming Project 1/ProgrammingExercise-3/Form1.cs
@@ -0,0 +1,62 @@
+﻿// Chad Weirick
+// Grand Canyon University
+// CST-117
+// Dr. J's class!
+//
+// This is a simple program designed to convert units of weight on Earth to those on Mars.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_117_Week_1_Assignment_3_and_Beyond
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            string myEarthWeightString = earthWeightTextBox.Text;
+            // this will reset to 0 if the text is invalid
+            double myEarthWeightDouble = checkDouble.parseDouble(myEarthWeightString);
+            double myMarsWeightDouble = myEarthWeightDouble / 9.81 * 3.711;
+            marsWeightTextBox.Text = myMarsWeightDouble.ToString("0.###");
+        }
+    }
+
+    public static class checkDouble
+    {
+        public static double parseDouble(this string value, double defaultValue = 0)
+        {
+            // initiate a return value
+            double results = 0;
+            if (double.TryParse(value, out results))
+            {
+                return results;
+            }
+            // if we made it this far, return 0
+            return defaultValue;
+        }
+
+        public static double? nullDouble(this string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+            // return the value through the parser.
+            return value.parseDouble();
+        }
+    }
+}
