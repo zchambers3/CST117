a55c173fd96642c37b352202a338f41e2f27197a
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-07-21 20:25:31 -0700

    Add files via upload

---
+++ Week 2/Exercise 4/Exercise04/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Exercise04
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.secondsEntryLabel = new System.Windows.Forms.Label();
+            this.secondsTextBox = new System.Windows.Forms.TextBox();
+            this.convertedLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // secondsEntryLabel
+            // 
+            this.secondsEntryLabel.AutoSize = true;
+            this.secondsEntryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsEntryLabel.Location = new System.Drawing.Point(13, 58);
+            this.secondsEntryLabel.Name = "secondsEntryLabel";
+            this.secondsEntryLabel.Size = new System.Drawing.Size(267, 25);
+            this.secondsEntryLabel.TabIndex = 0;
+            this.secondsEntryLabel.Text = "Enter the number of seconds:";
+            // 
+            // secondsTextBox
+            // 
+            this.secondsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsTextBox.Location = new System.Drawing.Point(307, 55);
+            this.secondsTextBox.Name = "secondsTextBox";
+            this.secondsTextBox.Size = new System.Drawing.Size(114, 30);
+            this.secondsTextBox.TabIndex = 1;
+            // 
+            // convertedLabel
+            // 
+            this.convertedLabel.AutoSize = true;
+            this.convertedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertedLabel.Location = new System.Drawing.Point(13, 105);
+            this.convertedLabel.Name = "convertedLabel";
+            this.convertedLabel.Size = new System.Drawing.Size(0, 25);
+            this.convertedLabel.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(228, 138);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(112, 38);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 196);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.convertedLabel);
+            this.Controls.Add(this.secondsTextBox);
+            this.Controls.Add(this.secondsEntryLabel);
+            this.Name = "MainForm";
+            this.Text = "Seconds to Hours";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label secondsEntryLabel;
+        private System.Windows.Forms.TextBox secondsTextBox;
+        private System.Windows.Forms.Label convertedLabel;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ Week 2/Exercise 4/Exercise04/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise04
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int seconds;
+
+            if (int.TryParse(secondsTextBox.Text, out seconds)) 
+            {
+                //minutes
+                if(seconds >= 60 && seconds < 3600)
+                {
+                    int minutes = seconds / 60;
+
+                    //Test for grammatical considerations.
+                    if(minutes == 1)
+                    {
+                        convertedLabel.Text = seconds.ToString() + " seconds contains " + minutes.ToString() + " minute.";
+                    }
+                    else
+                    {
+                        convertedLabel.Text = seconds.ToString() + " seconds contains " + minutes.ToString() + " minutes.";
+                    }
+                }
+                //hours
+                else if(seconds >= 3600 && seconds < 86400)
+                {
+                    int hours = seconds / 3600;
+
+                    if(hours == 1)
+                    {
+                        convertedLabel.Text = seconds.ToString() + " seconds contains " + hours.ToString() + " hour.";
+                    }
+                    else
+                    {
+                        convertedLabel.Text = seconds.ToString() + " seconds contains " + hours.ToString() + " hours.";
+                    }
+                    
+                }
+                //days
+                else if(seconds >= 86400)
+                {
+                    int days = seconds / 86400;
+
+                    if(days == 1)
+                    {
+                        convertedLabel.Text = seconds.ToString() + " seconds contains " + days.ToString() + " day";
+                    }
+                    else
+                    {
+                        convertedLabel.Text = seconds.ToString() + " seconds contains " + days.ToString() + " days";
+                    }
+                }
+                //less than 60 seconds
+                else
+                {
+                    convertedLabel.Text = "Enter a whole number greater than 59.";
+                }
+            }
+            else
+            {
+                //Could not parse the number because it is not an int value.
+                MessageBox.Show("Enter a valid integer number.");
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
