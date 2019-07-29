b475fd52617d27f38581677bca783bec7dfadd9e
zCoder <40376705+zchambers3@users.noreply.github.com>
2019-07-28 20:49:31 -0700

    Add files via upload

---
+++ Week 3/Programming Project 3/ProgrammingProject03/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ProgrammingProject03
+{
+    partial class StatsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statsListBox = new System.Windows.Forms.ListBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // statsListBox
+            // 
+            this.statsListBox.FormattingEnabled = true;
+            this.statsListBox.ItemHeight = 16;
+            this.statsListBox.Location = new System.Drawing.Point(13, 61);
+            this.statsListBox.Name = "statsListBox";
+            this.statsListBox.Size = new System.Drawing.Size(460, 244);
+            this.statsListBox.TabIndex = 0;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "*txt";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(113, 328);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(107, 33);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(259, 329);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(109, 32);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "This text is saved in a file named \"Statistics.txt\" within the same \r\ndirectory t" +
+    "hat the file was opened from.";
+            // 
+            // StatsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.statsListBox);
+            this.Name = "StatsForm";
+            this.Text = "Statistics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox statsListBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Week 3/Programming Project 3/ProgrammingProject03/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+//Zack Chambers
+
+//CST-117
+
+//07/28/2019
+
+namespace ProgrammingProject03
+{
+    public partial class StatsForm : Form
+    {
+        public StatsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            //check if a file was opened.
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Declare variables
+                String readText;
+                String[] currentWord;
+                String firstWordAlphabetically = ""; //Set to zzzzzzzz to ensure that nearly any value will precede when compared.
+                String lastWordAlphabetically = ""; //See above.
+                String longestWord = "";
+                String mostVowels = "";
+                char[] wordCharArray;
+                int currentVowelCount;
+                int mostVowelCount = 0;
+
+                //Clear the list box
+                statsListBox.Items.Clear();
+
+                try
+                {
+                    //Setup for file system.
+                    StreamReader textFile = new StreamReader(openFileDialog.FileName);
+                    StreamWriter saveFile = File.CreateText("Statistics.txt");
+
+                    while (!textFile.EndOfStream)
+                    {
+                        //load the file, convert it to lower case
+                        readText = textFile.ReadLine();
+                        readText = readText.ToLower();
+                        //Split the line into an array of Strings in case the line contains more than one word
+                        currentWord = readText.Split(' ');
+
+                        //loop to analyze the current word within the array
+                        for (int i = 0; i < currentWord.Length; i++)
+                        {
+
+                            currentVowelCount = 0;
+                            //Character array for searching for vowels.
+                            wordCharArray = currentWord[i].ToCharArray();
+                            
+                            //Compare the String in currentWord[i] to the current first word alphabetically.
+                            if (firstWordAlphabetically == "")
+                            {
+                                //Protection from initial value overriding the predicted outcome
+                                firstWordAlphabetically = currentWord[i];
+                            }
+                            else if (currentWord[i].CompareTo(firstWordAlphabetically) < 0)
+                            {
+                                //If it's first alphabetically, assign the word to its appropriate String variable.
+                                firstWordAlphabetically = currentWord[i];
+                            }
+                            
+                            if (lastWordAlphabetically == "")
+                            {
+                                lastWordAlphabetically = currentWord[i];
+                            }
+                            else if (currentWord[i].CompareTo(lastWordAlphabetically) > 0)
+                            {
+                                lastWordAlphabetically = currentWord[i];
+                            }
+
+                            //Find the longest word.
+                            if (longestWord == "")
+                            {
+                                longestWord = currentWord[i];
+                            }
+                            else if (longestWord.Length < currentWord[i].Length)
+                            {
+                                longestWord = currentWord[i];
+                            }
+
+                            //find any vowels within the word and count them
+                            for (int j = 0; j < wordCharArray.Length; j++)
+                            {
+                                if (wordCharArray[j] == 'a' ||
+                                wordCharArray[j] == 'e' ||
+                                wordCharArray[j] == 'i' ||
+                                wordCharArray[j] == 'o' ||
+                                wordCharArray[j] == 'u')
+                                {
+                                    currentVowelCount++;
+                                }
+                            }
+
+                            //Compare for the most vowels.
+                            if (currentVowelCount > mostVowelCount)
+                            {
+                                mostVowelCount = currentVowelCount;
+                                mostVowels = currentWord[i];
+                            }
+                        }
+                    }
+
+                    //Write items to the list box.
+                    statsListBox.Items.Add("First word alphabetically: " + firstWordAlphabetically);
+                    statsListBox.Items.Add("Last word alphabetically: " + lastWordAlphabetically);
+                    statsListBox.Items.Add("Longest word: " + longestWord + " at " + longestWord.Length + " characters.");
+                    statsListBox.Items.Add("Word with the most vowels: " + mostVowels + " with " + mostVowelCount + " vowels");
+
+                    //save the file to another file
+
+                    saveFile.WriteLine("First word alphabetically: " + firstWordAlphabetically);
+                    saveFile.WriteLine("Last word alphabetically: " + lastWordAlphabetically);
+                    saveFile.WriteLine("Longest word: " + longestWord + " at " + longestWord.Length + " characters.");
+                    saveFile.WriteLine("Word with the most vowels: " + mostVowels + " with " + mostVowelCount + " vowels");
+
+                    saveFile.Close();
+                    textFile.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
